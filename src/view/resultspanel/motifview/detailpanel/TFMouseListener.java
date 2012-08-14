269744013c305dd3ed34464552180ab18131ccf6
Bram <bram.vandesande@med.kuleuven.be>
2012-08-14 16:38:10 +0200

    iRegulon Java Client: refactoring.

--- src/view/resultspanel/motifview/detailpanel/TFMouseListener.java
+++ src/view/resultspanel/motifview/detailpanel/TFMouseListener.java
@@ -1,38 +1,38 @@
 package view.resultspanel.motifview.detailpanel;
 
-import view.resultspanel.PopupMenu;
-
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
-import javax.swing.JTable;
+import javax.swing.*;
 
 
-public class TFMouseListener extends MouseAdapter{
-
+public class TFMouseListener extends MouseAdapter {
 	private PopupMenu menu;
 	
 	public TFMouseListener(JTable table, TFandMotifSelected tfMotif){
-		if (table == null){
-			System.err.println("Couldn't create TFMouseListener");
+		if (table == null) {
+			throw new IllegalArgumentException();
 		}
 		menu = new PopupMenu();
 		final TFDetailFrameAction tfDetailFrameAction = new TFDetailFrameAction(tfMotif);
 		table.getSelectionModel().addListSelectionListener(tfDetailFrameAction);
 		menu.addAction(tfDetailFrameAction);
-
 	}
-	
-	
-	
-	/**
-	 * the mouseclicking
-	 */
+
 	public void mouseClicked(MouseEvent e){
-		if (e.getButton() == e.BUTTON3){
+		if (e.getButton() == MouseEvent.BUTTON3) {
 	        menu.show(e.getComponent(), e.getX(), e.getY());
 		}
 	}
-	
-	
+
+    private static class PopupMenu extends JPopupMenu {
+
+        public PopupMenu() {
+            super();
+        }
+
+        public void addAction(Action action) {
+            add(new JMenuItem(action));
+        }
+    }
 }

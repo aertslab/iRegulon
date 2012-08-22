932a0bac1e667a87a3ce6c0021f2a1d890d9f09f
Bram <bram.vandesande@med.kuleuven.be>
2012-08-22 09:07:08 +0200

    Distributions without TRANSFAC.

--- src/view/resultspanel/motifview/detailpanel/LogoUtilities.java
+++ src/view/resultspanel/motifview/detailpanel/LogoUtilities.java
@@ -16,6 +16,7 @@ public final class LogoUtilities {
 
     private static final String LOGO_FOLDERNAME = bundle.getString("logo_folder");
     private static final String LOGO_EXTENSION = bundle.getString("logo_extension");
+    private static final String LOGO_NOT_AVAILABLE = bundle.getString("logo_not_available");
 
 	private static final double IMAGE_HEIGHT = 50.0;
 	private static final double IMAGE_WIDTH = 100.0;
@@ -27,13 +28,23 @@ public final class LogoUtilities {
 	    return LOGO_FOLDERNAME + "/" + motifName + "." + LOGO_EXTENSION;
 	}
 
+    private static ImageIcon notAvailableImage() {
+        final java.net.URL imageUrl = LogoUtilities.class.getResource(LOGO_NOT_AVAILABLE);
+	    if (imageUrl != null) {
+	        return new ImageIcon(imageUrl);
+	    } else {
+            logger.handleLog(LogLevel.LOG_ERROR, "Couldn't find file: " + LOGO_NOT_AVAILABLE);
+            return null;
+        }
+    }
+
 	public static ImageIcon createImageIcon(final String motifName) {
 		final java.net.URL imageUrl = LogoUtilities.class.getResource(getImagePath(motifName));
 	    if (imageUrl != null) {
 	        return new ImageIcon(imageUrl);
 	    } else {
 	        logger.handleLog(LogLevel.LOG_ERROR, "Couldn't find file: " + getImagePath(motifName));
-	        return null;
+	        return notAvailableImage();
 	    }
 	}
 

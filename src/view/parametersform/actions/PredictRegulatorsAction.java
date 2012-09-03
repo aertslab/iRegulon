a27cc16153f211daa59c3babfb6986ccfcde4ee7
Bram <bram.vandesande@med.kuleuven.be>
2012-09-03 13:26:41 +0200

    Predict factors: better error message.

--- src/view/parametersform/actions/PredictRegulatorsAction.java
+++ src/view/parametersform/actions/PredictRegulatorsAction.java
@@ -66,13 +66,48 @@ public class PredictRegulatorsAction extends ResourceAction {
     }
 
     private String deriveMessage(ServerCommunicationException e) {
-        if (e.getMessage().contains("The following genes were lost:")) {
+        if (e.getMessage().contains("The following genes\nwere lost:")) {
+            final StringBuilder builder = new StringBuilder();
+            builder.append("<html>");
 
+            final String[] components = e.getMessage().split(":");
+            builder.append(components[0].replace('\n', ' '));
+            builder.append(":<br>");
 
+            final List<String> IDs = extractIDs(components[1]);
+            if (IDs.size() < 10) {
+                builder.append(IDs.get(0));
+                for (String ID : IDs.subList(1, IDs.size())) {
+                    builder.append(" ");
+                    builder.append(ID);
+                }
+            } else {
+                builder.append(IDs.get(0));
+                for (String ID : IDs.subList(1, 5)) {
+                    builder.append(" ");
+                    builder.append(ID);
+                }
+                builder.append(" ...");
+                final int size = IDs.size();
+                for (String ID : IDs.subList(size-5, size)) {
+                    builder.append(" ");
+                    builder.append(ID);
+                }
+            }
 
-            return e.getMessage();
+            builder.append("</html>");
+            return builder.toString();
         } else {
             return e.getMessage();
         }
+    }
+
+    private List<String> extractIDs(final String text) {
+        final String[] IDs = text.split(";");
+        final List<String> result = new ArrayList<String>();
+        for (String ID : IDs) {
+            result.add(ID.replace("\n", "").trim());
+        }
+        return result;
     }
 }

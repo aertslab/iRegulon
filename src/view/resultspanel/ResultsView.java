0e5e0e6139f1abb6ca2a0c326b4b3db4f3dcc08c
Gert Hulselmans <gert.hulselmans@med.kuleuven.be>
2013-09-13 11:51:46 +0200

    Switch "Transcription Factor" and "Motif" view in results panel.
    Switch "Transcription Factor view" and "Motif view" in results panel
    so the motif view will become the default.
    
    Signed-off-by: Gert Hulselmans <gert.hulselmans@med.kuleuven.be>

--- src/view/resultspanel/ResultsView.java
+++ src/view/resultspanel/ResultsView.java
@@ -2,24 +2,21 @@ package view.resultspanel;
 
 import cytoscape.Cytoscape;
 import cytoscape.view.CytoscapeDesktop;
-import domainmodel.*;
+import cytoscape.view.cytopanels.CytoPanel;
+import domainmodel.AbstractMotif;
+import domainmodel.GeneIdentifier;
+import domainmodel.Results;
+import domainmodel.TranscriptionFactor;
 import view.IRegulonResourceBundle;
 import view.actions.LoadResultsAction;
 import view.actions.OpenQueryMetatargetomeFormAction;
 import view.actions.QueryMetatargetomeAction;
 import view.parametersform.DefaultMetatargetomeParameters;
+import view.resultspanel.actions.*;
 import view.resultspanel.guiwidgets.SummaryLabel;
 import view.resultspanel.guiwidgets.TranscriptionFactorComboBox;
+import view.resultspanel.motifclusterview.MotifClustersView;
 import view.resultspanel.motifview.EnrichedMotifsView;
-import view.resultspanel.actions.*;
-
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
 
 import javax.swing.*;
 import javax.swing.event.ChangeEvent;
@@ -27,13 +24,11 @@ import javax.swing.event.ChangeListener;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import javax.swing.text.JTextComponent;
-
-import view.resultspanel.actions.CreateNewRegulatoryNetworkAction;
-import view.resultspanel.actions.AddRegulatoryNetworkAction;
-
-import view.resultspanel.motifclusterview.MotifClustersView;
-
-import cytoscape.view.cytopanels.CytoPanel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
 
 public class ResultsView extends IRegulonResourceBundle implements Refreshable {
@@ -48,7 +43,7 @@ public class ResultsView extends IRegulonResourceBundle implements Refreshable {
     private TranscriptionFactorComboBox transcriptionFactorCB;
 
     private JButton closeButton;
-	private JPanel mainPanel = null;
+    private JPanel mainPanel = null;
     private JTabbedPane tabbedPane;
 
     private final DefaultMetatargetomeParameters parameters;
@@ -58,9 +53,9 @@ public class ResultsView extends IRegulonResourceBundle implements Refreshable {
     private AddRegulatoryNetworkAction drawNodesAndEdgesAction;
 
     public ResultsView(final String runName, final Results results) {
-		this.runName = runName;
+        this.runName = runName;
         this.results = results;
-		this.isSaved = false;
+        this.isSaved = false;
 
         this.parameters = new DefaultMetatargetomeParameters(QueryMetatargetomeAction.DEFAULT_PARAMETERS);
         this.selectedMotif = new SelectedMotif(results.getParameters().getAttributeName());
@@ -77,7 +72,7 @@ public class ResultsView extends IRegulonResourceBundle implements Refreshable {
             }
         };
         registerRefreshListeners();
-	}
+    }
 
     private void registerRefreshListeners() {
         Cytoscape.getDesktop().getSwingPropertyChangeSupport().addPropertyChangeListener(CytoscapeDesktop.NETWORK_VIEW_FOCUS, refreshListener);
@@ -122,14 +117,14 @@ public class ResultsView extends IRegulonResourceBundle implements Refreshable {
         if (this.mainPanel != null) {
             throw new IllegalStateException();
         }
-		final JPanel resultsView = createMainPanel();
+        final JPanel resultsView = createMainPanel();
         this.mainPanel = resultsView;
 
         panel.add(getPanelName(), resultsView);
 
-		// Make this new panel active ...
-		final int index = panel.indexOfComponent(resultsView);
-		panel.setSelectedIndex(index);
+        // Make this new panel active ...
+        final int index = panel.indexOfComponent(resultsView);
+        panel.setSelectedIndex(index);
 
         // Add listener for closing this results view ...
         getCloseButton().setAction(new CloseResultsViewAction(panel, this));
@@ -152,8 +147,8 @@ public class ResultsView extends IRegulonResourceBundle implements Refreshable {
     private JButton getCloseButton() {
         return closeButton;
     }
-	
-	private JPanel createMainPanel() {
+
+    private JPanel createMainPanel() {
         // 1. Create toolbar ...
         this.transcriptionFactorCB = new TranscriptionFactorComboBox(this.selectedMotif, this.results.getSpeciesNomenclature());
         this.filterAttributeCB = new JComboBox(FilterAttribute.values());
@@ -169,12 +164,12 @@ public class ResultsView extends IRegulonResourceBundle implements Refreshable {
 
         // 4. Create tabbed pane with these views ...
         tabbedPane = new JTabbedPane();
+        tabbedPane.addTab("Motifs", null,
+                motifsView,
+                "Motif oriented view.");
         tabbedPane.addTab("Transcription Factors", null,
                 tfsView,
                 "Transcription factor oriented view.");
-		tabbedPane.addTab("Motifs", null,
-                motifsView,
-                "Motif oriented view.");
         tabbedPane.addChangeListener(new ChangeListener() {
             private MotifView getOtherView(final MotifView view) {
                 if (view == motifsView) return tfsView;
@@ -212,12 +207,12 @@ public class ResultsView extends IRegulonResourceBundle implements Refreshable {
             view.registerFilterComponents(filterAttributeCB, filterValueTF);
         }
 
-		final JPanel result = new JPanel();
-		result.setLayout(new BorderLayout());
+        final JPanel result = new JPanel();
+        result.setLayout(new BorderLayout());
         result.add(toolBar, BorderLayout.NORTH);
         result.add(tabbedPane, BorderLayout.CENTER);
         return result;
-	}
+    }
 
     private JPanel createToolBar(final SelectedMotif selectedMotif, final TranscriptionFactorComboBox transcriptionFactorComboBox,
                                  final JButton closeButton, final JComboBox filterAttributeCB, final JTextField filterValueTF) {
@@ -225,32 +220,42 @@ public class ResultsView extends IRegulonResourceBundle implements Refreshable {
         final GridBagConstraints c = new GridBagConstraints();
 
         // First line ...
-        c.gridx = 0; c.gridy = 0;
-		c.weightx = 1.0; c.weighty = 0;
-		c.gridwidth = 7; c.gridheight = 1;
+        c.gridx = 0;
+        c.gridy = 0;
+        c.weightx = 1.0;
+        c.weighty = 0;
+        c.gridwidth = 7;
+        c.gridheight = 1;
         c.fill = GridBagConstraints.HORIZONTAL;
         c.anchor = GridBagConstraints.LINE_START;
         toolBar.add(new SummaryLabel(getResults()), c);
 
-        c.gridx = 8; c.gridy = 0;
-        c.weightx = 0.0; c.weighty = 0.0;
+        c.gridx = 8;
+        c.gridy = 0;
+        c.weightx = 0.0;
+        c.weighty = 0.0;
         c.fill = GridBagConstraints.NONE;
         c.anchor = GridBagConstraints.CENTER;
-		toolBar.add(closeButton, c);
+        toolBar.add(closeButton, c);
 
         // Second line ...
-        c.gridx = 0; c.gridy = 1;
-		c.weightx = 0.0; c.weighty = 0.0;
-        c.gridwidth = 1; c.gridheight = 1;
+        c.gridx = 0;
+        c.gridy = 1;
+        c.weightx = 0.0;
+        c.weighty = 0.0;
+        c.gridwidth = 1;
+        c.gridheight = 1;
         c.fill = GridBagConstraints.NONE;
         c.anchor = GridBagConstraints.CENTER;
-		drawNodesAndEdgesAction = new AddRegulatoryNetworkAction(selectedMotif, transcriptionFactorComboBox, this, results.getParameters().getAttributeName());
+        drawNodesAndEdgesAction = new AddRegulatoryNetworkAction(selectedMotif, transcriptionFactorComboBox, this, results.getParameters().getAttributeName());
         final JButton buttonDrawEdges = new JButton(drawNodesAndEdgesAction);
         buttonDrawEdges.setText("");
         toolBar.add(buttonDrawEdges, c);
 
-        c.gridx = 1; c.gridy = 1;
-		c.weightx = 0.0; c.weighty = 0.0;
+        c.gridx = 1;
+        c.gridy = 1;
+        c.weightx = 0.0;
+        c.weighty = 0.0;
         c.fill = GridBagConstraints.NONE;
         c.anchor = GridBagConstraints.CENTER;
         final TranscriptionFactorDependentAction drawNetworkAction = new CreateNewRegulatoryNetworkAction(selectedMotif, transcriptionFactorComboBox, this, results.getParameters().getAttributeName());
@@ -258,8 +263,10 @@ public class ResultsView extends IRegulonResourceBundle implements Refreshable {
         buttonDrawNetwork.setText("");
         toolBar.add(buttonDrawNetwork, c);
 
-        c.gridx = 2; c.gridy = 1;
-		c.weightx = 0.0; c.weighty = 0.0;
+        c.gridx = 2;
+        c.gridy = 1;
+        c.weightx = 0.0;
+        c.weighty = 0.0;
         c.fill = GridBagConstraints.NONE;
         c.anchor = GridBagConstraints.CENTER;
         final OpenQueryMetatargetomeFormAction queryMetatargetomeAction = new OpenQueryMetatargetomeFormAction(parameters, this);
@@ -271,8 +278,10 @@ public class ResultsView extends IRegulonResourceBundle implements Refreshable {
         buttonQueryMetatargetome.setText("");
         toolBar.add(buttonQueryMetatargetome, c);
 
-        c.gridx = 3; c.gridy = 1;
-        c.weightx = 0.0; c.weighty = 0.0;
+        c.gridx = 3;
+        c.gridy = 1;
+        c.weightx = 0.0;
+        c.weighty = 0.0;
         c.fill = GridBagConstraints.NONE;
         c.anchor = GridBagConstraints.WEST;
         createNewCombinedRegulatoryNetworkAction = new CreateNewCombinedRegulatoryNetworkAction(results.getParameters().getAttributeName(), this);
@@ -280,75 +289,91 @@ public class ResultsView extends IRegulonResourceBundle implements Refreshable {
         drawMergedButton.setText("");
         toolBar.add(drawMergedButton, c);
 
-        c.gridx = 4; c.gridy = 1;
-        c.weightx = 0.0; c.weighty = 0.0;
+        c.gridx = 4;
+        c.gridy = 1;
+        c.weightx = 0.0;
+        c.weighty = 0.0;
         c.fill = GridBagConstraints.NONE;
         c.anchor = GridBagConstraints.LINE_END;
-		final JLabel labelTF = new JLabel("TF");
+        final JLabel labelTF = new JLabel("TF");
         labelTF.setToolTipText("Transcription factor");
-		toolBar.add(labelTF, c);
+        toolBar.add(labelTF, c);
 
-        c.gridx = 5; c.gridy = 1;
-		c.weightx = 1.0; c.weighty = 0.0;
+        c.gridx = 5;
+        c.gridy = 1;
+        c.weightx = 1.0;
+        c.weighty = 0.0;
         c.fill = GridBagConstraints.HORIZONTAL;
         c.anchor = GridBagConstraints.CENTER;
-		transcriptionFactorComboBox.setEditable(true);
+        transcriptionFactorComboBox.setEditable(true);
         transcriptionFactorComboBox.setToolTipText("List of predicted transcription factors.");
         toolBar.add(transcriptionFactorComboBox, c);
 
-        c.gridx = 6; c.gridy = 1;
-        c.weightx = 0.0; c.weighty = 0.0;
+        c.gridx = 6;
+        c.gridy = 1;
+        c.weightx = 0.0;
+        c.weighty = 0.0;
         c.fill = GridBagConstraints.NONE;
         c.anchor = GridBagConstraints.CENTER;
         final JButton buttonLoad = new JButton(new LoadResultsAction());
         buttonLoad.setText("");
         toolBar.add(buttonLoad, c);
 
-        c.gridx = 7; c.gridy = 1;
-        c.weightx = 0.0; c.weighty = 0.0;
+        c.gridx = 7;
+        c.gridy = 1;
+        c.weightx = 0.0;
+        c.weighty = 0.0;
         c.fill = GridBagConstraints.NONE;
         c.anchor = GridBagConstraints.CENTER;
         final JButton buttonSave = new JButton(new SaveResultsAction(this));
         buttonSave.setText("");
         toolBar.add(buttonSave, c);
 
-        c.gridx = 8; c.gridy = 1;
-		c.weightx = 0.0; c.weighty = 0.0;
+        c.gridx = 8;
+        c.gridy = 1;
+        c.weightx = 0.0;
+        c.weighty = 0.0;
         c.fill = GridBagConstraints.NONE;
         c.anchor = GridBagConstraints.CENTER;
-		final JButton buttonTabDelimited = new JButton(new ExportResultsAction(this));
+        final JButton buttonTabDelimited = new JButton(new ExportResultsAction(this));
         buttonTabDelimited.setText("");
-		toolBar.add(buttonTabDelimited, c);
+        toolBar.add(buttonTabDelimited, c);
 
         // Third line ...
-		c.gridx = 0; c.gridy = 2;
-        c.weightx = 0.0; c.weighty = 0.0;
+        c.gridx = 0;
+        c.gridy = 2;
+        c.weightx = 0.0;
+        c.weighty = 0.0;
         c.fill = GridBagConstraints.NONE;
         c.anchor = GridBagConstraints.CENTER;
         toolBar.add(new JLabel(loadIcon("filter_icon")), c);
 
-		c.gridx = 1; c.gridy = 2;
-		c.gridwidth = 3;
-		c.weightx = 1.0; c.weighty = 0.0;
+        c.gridx = 1;
+        c.gridy = 2;
+        c.gridwidth = 3;
+        c.weightx = 1.0;
+        c.weighty = 0.0;
         c.fill = GridBagConstraints.HORIZONTAL;
         c.anchor = GridBagConstraints.CENTER;
         filterAttributeCB.setToolTipText("Select feature to filter on.");
-		toolBar.add(filterAttributeCB, c);
+        toolBar.add(filterAttributeCB, c);
 
-		c.gridx = 4; c.gridy = 2;
-		c.gridwidth = 5;
-		c.weightx = 1.0; c.weighty = 0.0;
+        c.gridx = 4;
+        c.gridy = 2;
+        c.gridwidth = 5;
+        c.weightx = 1.0;
+        c.weighty = 0.0;
         c.fill = GridBagConstraints.HORIZONTAL;
         c.anchor = GridBagConstraints.CENTER;
         filterValueTF.setToolTipText("Filter the main view table on motif, transcription factor or target gene.");
-		toolBar.add(filterValueTF, c);
+        toolBar.add(filterValueTF, c);
 
         return toolBar;
     }
 
     private ImageIcon loadIcon(final String keyName) {
         final String resourceName = getBundle().getString(keyName);
-		return new ImageIcon(getClass().getResource(resourceName));
+        return new ImageIcon(getClass().getResource(resourceName));
     }
 
     private static class QueryMetatargetomeActionListener implements ActionListener {
@@ -381,7 +406,7 @@ public class ResultsView extends IRegulonResourceBundle implements Refreshable {
         private final TranscriptionFactorComboBox transcriptionFactorComboBox;
 
         public QueryMetatargetomeDocumentListener(final OpenQueryMetatargetomeFormAction queryMetatargetomeAction,
-                                                   final DefaultMetatargetomeParameters parameters,
+                                                  final DefaultMetatargetomeParameters parameters,
                                                   TranscriptionFactorComboBox transcriptionFactorComboBox) {
             this.queryMetatargetomeAction = queryMetatargetomeAction;
             this.parameters = parameters;

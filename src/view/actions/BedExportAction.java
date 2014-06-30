6b707a8f9ea85ee787a57ccba66ec6951dca9e81
Gert Hulselmans <gert.hulselmans@med.kuleuven.be>
2014-06-30 14:07:38 +0200

    Use PersistenceViewUtilities.saveToSelectedFile() in view.actions.BedExportAction.
    Use PersistenceViewUtilities.saveToSelectedFile() in view.actions.BedExportAction.
    
    Signed-off-by: Gert Hulselmans <gert.hulselmans@med.kuleuven.be>

--- src/view/actions/BedExportAction.java
+++ src/view/actions/BedExportAction.java
@@ -13,16 +13,10 @@ import view.resultspanel.MotifAndTrackListener;
 import view.resultspanel.SelectedMotifOrTrack;
 
 import javax.swing.*;
-import javax.swing.filechooser.FileFilter;
-import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.event.ActionEvent;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 
 
-public class BedExportAction extends ResourceAction implements Refreshable {
+public final class BedExportAction extends ResourceAction implements Refreshable {
     private static final String NAME = "action_save_bed";
 
     private final ComputationalService service = new ComputationalServiceHTTP();
@@ -54,60 +48,20 @@ public class BedExportAction extends ResourceAction implements Refreshable {
 
     @Override
     public void actionPerformed(ActionEvent event) {
-        final JFileChooser fc = new JFileChooser(new File(getPathSuggestion()));
-        // Display "All Files" filter at the bottom of the list.
-        fc.setAcceptAllFileFilterUsed(false);
-        fc.addChoosableFileFilter(new FileNameExtensionFilter("UCSC BED files (*.bed)", "bed"));
-        fc.setAcceptAllFileFilterUsed(true);
+        final StringBuilder bed_output = new StringBuilder();
 
-        fc.setSelectedFile(new File(getFilenameSuggestion()));
-        fc.showSaveDialog(Cytoscape.getDesktop());
-        FileFilter ff = fc.getFileFilter();
-
-        final File selectedFile = ff.accept(fc.getSelectedFile())
-                ? fc.getSelectedFile()
-                : new File(fc.getSelectedFile().getAbsoluteFile() + "." + FileTypes.BED.getExtensions()[0]);
-
-        BufferedWriter output = null;
         try {
-            if (!selectedFile.createNewFile()) {
-                // File already exists ...
-                int n = JOptionPane.showConfirmDialog(Cytoscape.getDesktop(),
-                        "Do you want to overwrite the file '" + selectedFile.getName() + "'?",
-                        "Overwrite?",
-                        JOptionPane.YES_NO_OPTION);
-                if (n == JOptionPane.NO_OPTION) {
-                    return;
-                }
+            for (EnhancerRegion region : service.getEnhancerRegions(selectedMotifOrTrack.getMotifOrTrack())) {
+                bed_output.append(region.toString() + "\n");
             }
-            output = new BufferedWriter(new FileWriter(selectedFile));
-            try {
-                for (EnhancerRegion region : service.getEnhancerRegions(selectedMotifOrTrack.getMotifOrTrack())) {
-                    output.write(region.toString());
-                    output.write('\n');
-                }
-                output.flush();
-            } catch (ServerCommunicationException e) {
-                Logger.getInstance().error(e.getMessage());
-                JOptionPane.showMessageDialog(Cytoscape.getDesktop(),
-                        "An error has occurred while communicating with server.",
-                        "Error",
-                        JOptionPane.ERROR_MESSAGE);
-            }
-        } catch (IOException e) {
+        } catch (ServerCommunicationException e) {
             Logger.getInstance().error(e.getMessage());
-            JOptionPane.showMessageDialog(Cytoscape.getDesktop(),
-                    "An error has occurred while creating and saving the BED file '" + selectedFile.getName() + ".",
+            JOptionPane.showMessageDialog(CytoscapeEnvironment.getInstance().getJFrame(),
+                    "An error has occurred while communicating with server.",
                     "Error",
                     JOptionPane.ERROR_MESSAGE);
-        } finally {
-            if (output != null) {
-                try {
-                    output.close();
-                } catch (IOException e) {
-                    //Nop ...
-                }
-            }
         }
+
+        PersistenceViewUtilities.saveToSelectedFile(bed_output.toString(), FileTypes.BED);
     }
 }

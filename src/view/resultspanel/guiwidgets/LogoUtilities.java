cc22c1db0916e9b0998236376fb8fc772d1296df
Gert Hulselmans <gert.hulselmans@med.kuleuven.be>
2015-09-18 15:55:10 +0200

    Add support for displaying motifs of different motif collections.
    Add support for displaying motifs of different motif collections
    and fix exclusion of TRANFSFAC Pro motif logos in without TRANSFAC
    jar and source archives for motif collection v7.
    
    Signed-off-by: Gert Hulselmans <gert.hulselmans@med.kuleuven.be>

--- src/view/resultspanel/guiwidgets/LogoUtilities.java
+++ src/view/resultspanel/guiwidgets/LogoUtilities.java
@@ -12,14 +12,19 @@ import java.util.ResourceBundle;
 public final class LogoUtilities {
     private static final ResourceBundle RESOURCE_BUNDLE = IRegulonResourceBundle.getBundle();
 
-    private static final String LOGO_FOLDERNAME = RESOURCE_BUNDLE.getString("logo_folder");
+    private static final String MC_V3_V6_LOGO_FOLDERNAME = RESOURCE_BUNDLE.getString("mc_v3_v6_logo_folder");
+    private static final String MC_V7_LOGO_FOLDERNAME = RESOURCE_BUNDLE.getString("mc_v7_logo_folder");
     private static final String LOGO_EXTENSION = RESOURCE_BUNDLE.getString("logo_extension");
 
     private LogoUtilities() {
     }
 
     private static String getImagePath(String motifName) {
-        return LOGO_FOLDERNAME + "/" + motifName + "." + LOGO_EXTENSION;
+        if (motifName.indexOf("__") < motifName.indexOf('-')) {
+            return MC_V7_LOGO_FOLDERNAME + "/" + motifName + "." + LOGO_EXTENSION;
+        } else {
+            return MC_V3_V6_LOGO_FOLDERNAME + "/" + motifName + "." + LOGO_EXTENSION;
+        }
     }
 
     public static java.net.URL getImageFileURL(final String motifName) {
